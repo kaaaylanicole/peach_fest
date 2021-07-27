94f30ccc4b00126fff59fefbce50b6f597010df4
Kayla Coursey <kaaayla08@gmail.com>
2021-07-27 16:46:55 -0400

    added comments to code

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+// JS for toggling between days 
 function toggleDays(day) {
     let dayWrappers = document.getElementsByClassName('day-wrapper')
     dayWrappers = Array.prototype.slice.call( dayWrappers )
@@ -12,11 +13,13 @@ function toggleDays(day) {
     })
   }
 
+// JS for clicking on Buy Tickets button and Contact Form to pop up
 function showForm() {
   let formDiv = document.getElementById('contact-form')
   formDiv.classList.remove('hide')
 }
 
+// JS for clicking "X" on form and hiding the popup module
 function hideForm() {
   let formDiv = document.getElementById('contact-form')
   formDiv.classList.add('hide')
